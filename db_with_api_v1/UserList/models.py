34d901ad8144f0df55c41f63d15abf4e57434402
Jornped <sasha.ahmat12@gmail.com>
2020-02-22 18:19:03 +0500

    api_v1.2

--- db_with_api_v1/UserList/models.py
+++ db_with_api_v1/UserList/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    user_id = models.IntegerField(null=False, primary_key=True)
-    nickname = models.CharField(max_length=64, unique=True, null=False)
+    userName = models.CharField(max_length=32, null=False, primary_key=True)
+    email = models.CharField(max_length=64, unique=True, null=False)
     password = models.CharField(max_length=32, null=False)
 
 
